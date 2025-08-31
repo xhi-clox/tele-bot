a361cad31b1e27b54937f9e1957cd8e50c1c27e9
xhi-clox <twarkay@gmail.com>
2025-08-31 14:04:32 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+  distDir: '.next',
+  dir: 'src',
   images: {
     remotePatterns: [
       {
